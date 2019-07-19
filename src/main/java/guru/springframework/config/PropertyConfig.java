41e0a678b0b39809c2e19fe2cc55835cbb2d5076
Marcin <marcin96semkowski@gmail.com>
2019-07-19 15:44:55 +0200

    Many Property Config

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,16 +1,20 @@
 package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+@PropertySources({@PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
         @Autowired
     Environment env;
@@ -19,6 +23,14 @@ public class PropertyConfig {
      @Value("${guru.dburl}")
     String url;
 
+
+     @Value("${guru.jms.username}")
+     String jmsUsername;
+     @Value("${guru.jms.password}")
+     String jmsPassword;
+     @Value("${guru.jms.url}")
+     String jmsUrl;
+
        @Bean
      public FakeDataSource fakeDataSource(){
          FakeDataSource fakeDataSource = new FakeDataSource();
@@ -26,6 +38,16 @@ public class PropertyConfig {
          fakeDataSource.setPassword(password);
          fakeDataSource.setUrl(url);
          return fakeDataSource;
+     }
+
+     @Bean
+     public FakeJmsBroker fakeJmsBroker(){
+         FakeJmsBroker jmsBroker = new FakeJmsBroker();
+
+         jmsBroker.setUsername(jmsUsername);
+         jmsBroker.setPassword(jmsPassword);
+         jmsBroker.setUrl(jmsUrl);
+         return jmsBroker;
      }
 
 
